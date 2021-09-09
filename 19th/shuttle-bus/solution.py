80035fe647b3ad65ba2964f74b7eeb290c8ddd01
WooJin1993 <ahnwj2004@gmail.com>
2021-09-09 13:12:01 +0900

    solve shuttle-bus

--- 19th/shuttle-bus/solution.py
+++ 19th/shuttle-bus/solution.py
@@ -6,18 +6,30 @@
 from copy import deepcopy
 from heapq import heapify, heappop, heappush
 
+def time2num(time):
+    
+    return 60*int(time[:2]) + int(time[3:])
+
+def num2time(num):
+    q, r = map(str, divmod(num, 60))
+    
+    return f"{q.zfill(2)}:{r.zfill(2)}"
+    
 def solution(n, t, m, timetable):
-    timetable = [60*int(time[:2]) + int(time[3:]) for time in timetable]
-    last_bus = 540 + t*(n-1)
+    timetable = [time2num(time) for time in timetable]
+    last_bus = 540
+    
+    for _ in range(n - 1):
+        if last_bus + t <= 60*23 + 58:
+            last_bus += t
+            
     timetable = [(time, 0) for time in timetable if time <= last_bus]
-    
-    if not timetable:
-        q, r = divmod(last_bus, 60)
-        
-        return f"{str(q).zfill(2)}:{str(r).zfill(2)}"
-    
     heapify(timetable)
     result = []
+    
+    if not timetable:
+        
+        return num2time(last_bus)
     
     for arrival in range(timetable[0][0] - 1, last_bus + 1):
         table_copy = deepcopy(timetable)
@@ -26,6 +38,7 @@ def solution(n, t, m, timetable):
         
         for _ in range(n):
             limit = m
+            flag = False
             
             while limit:
                 if table_copy[0][0] > shuttle:
@@ -37,10 +50,59 @@ def solution(n, t, m, timetable):
                 
                 if con:
                     result.append(arrival)
+                    flag = True
                     break
+            
+            if flag:
+                break
             
             shuttle += t
     
-    q, r = divmod(max(result), 60)
+    return num2time(max(result))
+
+# --- 수정된 풀이 ---
+
+from heapq import heapify, heappop
+
+def time2num(time):
     
-    return f"{str(q).zfill(2)}:{str(r).zfill(2)}"
+    return 60*int(time[:2]) + int(time[3:])
+
+def num2time(num):
+    q, r = map(str, divmod(num, 60))
+    
+    return f"{q.zfill(2)}:{r.zfill(2)}"
+    
+def solution(n, t, m, timetable):
+    timetable = [time2num(time) for time in timetable]
+    last_arrival = 540 + (n-1)*t
+    bus_arrival = 540
+    timetable = [time for time in timetable if time <= last_arrival]
+    heapify(timetable)
+    
+    if not timetable:
+        
+        return num2time(last_arrival)
+    
+    while bus_arrival <= last_arrival:
+        limit = m
+    
+        while limit:
+            limit -= 1
+            
+            if timetable[0] > bus_arrival:
+                break
+            
+            crew_arrival = heappop(timetable)
+            
+            if not timetable and bus_arrival < last_arrival:
+                return num2time(last_arrival)
+            
+            if bus_arrival == last_arrival:
+                if not timetable and limit >= 1:
+                    return num2time(last_arrival)
+                
+                if limit == 0:
+                    return num2time(crew_arrival - 1)
+                        
+        bus_arrival += t
