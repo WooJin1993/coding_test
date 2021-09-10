a3f42c96b55e0c73084b6fe9d07fb731c39b086d
WooJin1993 <ahnwj2004@gmail.com>
2021-09-10 16:57:07 +0900

    add 22th

---
+++ 22th/auto-completion/solution.py
@@ -0,0 +1,5 @@
+# 문제: https://programmers.co.kr/learn/courses/30/lessons/17685
+
+def solution(words):
+    answer = 0
+    return answer
---
+++ 22th/block-game/solution.py
@@ -0,0 +1,5 @@
+# 문제: https://programmers.co.kr/learn/courses/30/lessons/42894
+
+def solution(board):
+    answer = 0
+    return answer
